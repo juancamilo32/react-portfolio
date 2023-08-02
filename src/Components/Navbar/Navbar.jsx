28fa87b9d986af0458c52380b2d9f39b50f2cb4d
Juan Londoño <57278226+juancamilo32@users.noreply.github.com>
2023-08-02 17:27:04 -0500

    Navigation bar implemented

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import { Link, useLocation } from 'react-router-dom'
+import { HiHome, HiUser, HiViewColumns, HiEnvelope } from "react-icons/hi2";
+
+import './Navbar.css'
+
+const links = [
+    { name: 'home', path: '/', icon: <HiHome /> },
+    { name: 'about', path: '/about', icon: <HiUser /> },
+    { name: 'projects', path: '/projects', icon: <HiViewColumns /> },
+    { name: 'contact', path: '/contact', icon: <HiEnvelope />, }
+];
+
 const Navbar = () => {
+    const location = useLocation();
     return (
-        <div>Navbar</div>
+        <nav className='app-navbar'>
+            <div className='app-navbar-items'>
+                {links.map((link, index) => {
+                    return <Link to={link.path} key={index}>
+                        <div className={`${link.path === location.pathname ? "red-color" : "white-color"} app-navbar-items-icon`}>{link.icon}</div>
+                    </Link>
+                })}
+            </div>
+        </nav>
     )
 }
 
