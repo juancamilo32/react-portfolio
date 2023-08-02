24398566c3c36b5f25cb76344db9f722b3beebc8
Juan Londoño <57278226+juancamilo32@users.noreply.github.com>
2023-08-02 11:18:12 -0500

    Page routing implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { Home, About, Projects, Contact } from './routes';
+
+import { Header, Navbar } from './Components'
+
+const Layout = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    Hola
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
