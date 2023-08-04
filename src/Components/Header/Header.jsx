326e5f66282a97681058b3a8175e2c310f11b3c8
Juan Londoño <57278226+juancamilo32@users.noreply.github.com>
2023-08-04 10:21:11 -0500

    Header section implemented

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import './Header.css'
+import { logo } from '../../assets'
+
+import { RiGithubFill, RiLinkedinBoxFill } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='app-header'>
+            <div className='app-header-container'>
+                <img src={logo} />
+                <div className='app-header-container-socials'>
+                    <Link to={'https://github.com/juancamilo32'} className='link'><RiGithubFill className='red-hover' fontSize={24} /></Link>
+                    <Link to={'https://www.linkedin.com/in/juan-camilo-londoño/'} className='link'><RiLinkedinBoxFill className='red-hover' fontSize={24} /></Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,12 +14,13 @@ const links = [
 
 const Navbar = () => {
     const location = useLocation();
+
     return (
         <nav className='app-navbar'>
             <div className='app-navbar-items'>
                 {links.map((link, index) => {
-                    return <Link to={link.path} key={index}>
-                        <div className={`${link.path === location.pathname ? "red-color" : "white-color"} app-navbar-items-icon`}>{link.icon}</div>
+                    return <Link to={link.path} key={index} onClick={''}>
+                        <div className={`${link.path === location.pathname ? "red-color" : "white-color"} red-hover`}>{link.icon}</div>
                     </Link>
                 })}
             </div>
