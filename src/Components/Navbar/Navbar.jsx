854749de46bf338389420b7d2f2ba49a9e739b72
Juan Londoño <57278226+juancamilo32@users.noreply.github.com>
2023-09-04 11:04:44 -0500

    Navbar names displayed on hover

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -19,7 +19,13 @@ const Navbar = () => {
         <nav className='app-navbar'>
             <div className='app-navbar-items'>
                 {links.map((link, index) => {
-                    return <Link to={link.path} key={index} onClick={''}>
+                    return <Link to={link.path} key={index} onClick={''} className='link'>
+                        <div className='app-navbar-items-tooltip'>
+                            <div className='app-navbar-items-tooltip-rectangle'>
+                                <div className='app-navbar-items-tooltip-text'>{link.name}</div>
+                                <div className='app-navbar-items-tooltip-triangle'></div>
+                            </div>
+                        </div>
                         <div className={`${link.path === location.pathname ? "red-color" : "white-color"} red-hover`}>{link.icon}</div>
                     </Link>
                 })}
